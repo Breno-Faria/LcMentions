03ffcf25552313b0d54c43756727e6c05aa5e3c2
Brenofaria <brenofariac@gmail.com>
2022-07-11 18:08:23 -0400

    script to populate the database created

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import requests
+import psycopg2
+from db import conn
 
 # Getting the count of problems mentions on Reddit
-response = requests.get("https://api.pushshift.io/reddit/search/submission/?q=%22leetcode%201234%22&subreddit=leetcode&fields=title&after12m")
-if response.status_code == 200:
-	res = (response.json())
-count = {}
-for i in range(3):
-	count[len(requests.get(f"https://api.pushshift.io/reddit/search/submission/?q=%22leetcode%20{i}%22&subreddit=leetcode&fields=title&after12m").json()["data"])] = i
-for key, value in count.items():
-	print(f"Question {value} was mentioned {key} times")
+cur = conn.cursor()
+for i in range(10):
+	response = requests.get(f"https://www.google.com/search?q=leetcode+{i}")
+	l = (response.text.find("<h3"))
+	r = (response.text.find("h3>"))
+	cur.execute("INSERT INTO problems (count, number, title) VALUES (%s, %s, %s)",
+	(len(requests.get(f"https://api.pushshift.io/reddit/search/submission/?q=%22leetcode%20{i}%22&subreddit=leetcode&fields=title&after12m").json()["data"])
+	, i, response.text[l + 60:r - 19]))
 
-
-#Getting the LC question title/url
-response = requests.get("https://www.google.com/search?q=leetcode+124")
-
-l = (response.text.find("<h3"))
-r = (response.text.find("h3>"))
-print(response.text[l + 60:r - 8])
+conn.commit()
+cur.close()
+conn.close()
